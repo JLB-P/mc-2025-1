40401a87395318050aed59748436e7737e60f200
jlb <JLB-P@users.noreply.github.com>
2024-10-07 15:53:08 -0600

    load071024

---
+++ atmega328p/atmega328p/main.c
@@ -0,0 +1,18 @@
+/*
+ * atmega328p.c
+ *
+ * Created: 10/7/2024 3:49:43 PM
+ * Author : josel
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
